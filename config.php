7ffa7be01789d8486636abc5a342dcf0ff6e571a
Ilias Blatsiotis <66378565+BlatsGR@users.noreply.github.com>
2025-05-04 00:56:05 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('API_KEY', 'YOURAPIKEY(whatever)');
+define('DOMAINS_LIST', '/home/YOURUSER/web/YOURDOMAIN/private/domains.list');
+define('CERT_STATUS_JSON', '/home/YOURUSER/web/YOURDOMAIN/private/cert_status.json');
+define('LOGIN_USER', 'admin');
+define('LOGIN_PASS', 'admin');
+define('SESSION_TIMEOUT', 900);
